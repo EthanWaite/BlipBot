9a708c0bf2afb954723733dafa39c0f96edc0095
Dead-i <ethan.waite@btinternet.com>
2015-03-16 21:29:38 +0000

    Add better link parsing. Closes #3

--- commands/links.js
+++ commands/links.js
@@ -23,18 +23,37 @@ function dataHandler(data) {
 	for (var i in data.raw) {
 		var part = data.raw[i];
 		if (part.type == 'link') {
-			request(part.url, function(err, res, body) {
-				if (err) {
-					return;
-				}
-				
-				var title = cheerio.load(body)('head title').text();
-				if (title.length == 0) {
-					return;
-				}
-				
-				self.sendMessage(title, data.user.name);
-			});
+			if (/(http(s)?:\/\/beam.pro\/)([A-Za-z0-9]+)(\/)?/ig.test(part.url)) {
+				request('https://beam.pro/api/v1/channels/' + part.url.split('/')[3], function(err, res, body) {
+					if (err || res.statusCode !== 200) {
+						return;	
+					}
+					
+					var channel = JSON.parse(body);
+					self.sendMessage('(' + (channel.online ? 'Online' : 'Offline') + ') ' + channel.name, data.user.name);
+				});
+			}else{
+				request(part.url, function(err, res, body) {
+					if (err) {
+						return;
+					}
+
+					var $ = cheerio.load(body);
+					var title = $('head meta[property="og:title"]').attr('content');
+					if (title) {
+						var description = $('head meta[property="og:description"]').attr('content');
+						if (description) {
+							title = title + ': ' + description;	
+						}
+					}else{
+						title = $('head title').text();
+						if (title.length == 0) {
+							return;
+						}
+					}
+					self.sendMessage(title.substring(0, 200), data.user.name);
+				});
+			}
 			
 			break;
 		}
